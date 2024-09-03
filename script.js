4d22a3a127c42a410a63ed089bd2b4913964954a
HemantDahiyaCodes <HemnatDahiyaCodes@gmail.com>
2024-09-03 21:30:29 +0530

    .

---
+++ script.js
@@ -0,0 +1,30 @@
+const addTaskBtn = document.querySelector("button");
+const taskContainer = document.getElementById("task-container");
+const userInput = document.getElementById("addtask");
+const task = document.querySelector(".task");
+const ul = document.querySelector("ul");
+const addTask = () => {
+  if (userInput.value === "") {
+    alert("Plese enter at least one task to continue.");
+    return;
+  }
+
+  const li = document.createElement("li");
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Delete";
+
+  removeBtn.addEventListener("click", () => {
+    li.remove();
+  });
+  li.textContent = userInput.value;
+  li.appendChild(removeBtn);
+
+  ul.appendChild(li);
+  ul.style.width = "100%";
+  li.style.display = "flex";
+  li.style.justifyContent = "space-between";
+  li.style.alignItems = "center";
+  userInput.value = "";
+};
+
+addTaskBtn.addEventListener("click", addTask);
